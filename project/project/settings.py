78c0dc5960ca1c2cfde2b6fda22cdf1a2963e9fb
yiqizou001 <1099297317@qq.com>
2018-07-09 14:09:24 +0800

    添加env

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'axf',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'gz1801axf',
+        'USER':'ROOT',
+        'PASSWORD':'123546',
+        'HOST':'120.79.45.151',
+        'PORT':3306,
+
     }
 }
 
